bf4d507bf63dddbfce28ef1f4fb3ceb7cf7ea3ab
noob <aaeicsa@gmail.com>
2024-11-19 00:18:02 +0330

    change structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,62 @@
-#include <stdlib.h>
-#include <cstring>
-#include <string>
-#include <fmt/format.h>
-#include <nlohmann/json.hpp>
 #include <iostream>
+#include <string>
+#include <unordered_set>
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
 
 void
-show_help()
+usage(char* argv)
 {
-    std::cout << "tt | command line tasktracker"              << std::endl << std::endl
-              << "tt add \"new task\""                        << std::endl
-              << "tt update <task id> \"redefine task\""      << std::endl
-              << "tt remove <task id>"                        << std::endl << std::endl
-              << "tt done <task id>"                          << std::endl
-              << "tt wip <task id>"                           << std::endl
-              << "tt list [wip | done | notdone]"             << std::endl
-              ;
+    cout << "Usage: " 
+         << "(add | remove | update) <task> " << endl
+         << "       (list | listw | listd) <id> " << endl
+         << "       (markd | markw) <id> " << endl
+         << "       (clear | reset) " << endl;
 }
 
-void 
-add(char* argv[]) 
+int
+main(int argc, char* argv[])
 {
-    std::cout << "good to go!" << std::endl;
-    std::cout << argv[2] << std::endl;
-}
-
-bool
-is_command(std::string cmd, char* argv[])
-{
-    if(strcmp(argv[1], cmd.c_str()) == 0)
+    if(argc < 2)
     {
-        return true;
+        cerr << "Error: expected more arguments." << endl;
+        usage(argv[0]);
+        return -1;
+    }
+    
+    std::unordered_set<std::string> commands =
+    {
+        "add",
+        "remove",
+        "update",
+        
+        "list",
+        "listw",
+        "listd",
+
+        "markd",
+        "markw",
+
+        "clear",
+        "reset"
+    };
+
+    if(commands.count(argv[1]) > 0)
+    {
+        cout << "good to go!" << endl;
+        if(std::string(argv[1]) == "add")
+        {
+            cout << "add found" << endl;
+        }
     }
     else
     {
-        return false;
-    }
-
-}
-
-int 
-main(int argc, char* argv[])
-{
-    if (argc < 3)
-    {
-        std::cerr << "Usage: tt [command] <arguments>" << std::endl;
-        std::cerr << "Try 'tt --help' for more information." << std::endl;
+        cout << "Error: Invalid command." << endl;
+        usage(argv[0]);
         return -1;
-    }
-    else 
-    {
-        if(is_command("add", argv))
-        {
-            add(argv);
-        }
-        else 
-        {
-            std::cerr << "Invalid command. " << std::endl;
-            std::cerr << "Try 'tt --help' for more information." << std::endl;
-            return -1;
-        }
     }
 
     return 0;
