c03922a177050e3135fab352d994932ffad96480
noob <aaeicsa@gmail.com>
2024-12-02 10:42:04 +0330

    finish markd and markw

--- src/main.cpp
+++ src/main.cpp
@@ -307,6 +307,159 @@ int remove(int argc, char* argv[], const string path)
     }
 }
 
+int markdw(int argc, char* argv[], const string path, string time)
+{
+    if(argc == 3)
+    {
+        json data;
+        ifstream read_file;
+        read_file.open(path);
+
+        if(read_file)
+        {
+            try
+            {
+                data = json::parse(read_file); 
+            }
+            catch(const exception& e)
+            {
+                cout << "Error parsing data at: " 
+                    << path << endl;
+
+                return -1;
+            }
+        }
+        else 
+        {
+            cerr << "Unable to open tasks data at " 
+                << path << endl;
+
+            return -1;
+        }
+
+        bool updated = false;
+
+        for(auto task = data["tasks"].begin(); task != data["tasks"].end(); )
+        {
+            if(task->contains("id") && task->at("id") == atoi(argv[2]))
+            {
+                if(task->contains("status"))
+                {
+                    if(string(argv[1]) == "markw")
+                    {
+                        try
+                        {
+                            cout << "Marking: "<< task->at("description") << endl;
+                            task->at("status") = "wip";
+                            task->at ("updated") = time;
+                            updated = true;
+                            cout << "Task was marked as wip!" << endl;
+                            break;
+                        }
+                        catch(const exception& e)
+                        {
+                            cerr << "Something went wrong!" << endl;
+
+                            return -1;
+                        }
+                    }
+                    if(string(argv[1]) == "markd")
+                    {
+                        try
+                        {
+                            cout << "Marking: "<< task->at("description") << endl;
+                            task->at("status") = "done";
+                            task->at ("updated") = time;
+                            updated = true;
+                            cout << "Task was marked as done!" << endl;
+                            break;
+                        }
+                        catch(const exception& e)
+                        {
+                            cerr << "Something went wrong!" << endl;
+
+                            return -1;
+                        }
+
+                    }
+                }
+                else
+                {
+                    cerr << "There was something wrong with the tasks data" << endl;
+
+                    return -1;
+                }
+            }
+            else
+            {
+                ++task;
+            }
+        }
+
+        if(!updated)
+        {
+            cerr << "Could not update task with provided id!" << endl
+                << "The id you provided is likely not a valid id or integer."
+                << endl;
+
+            return -1;
+        }
+
+        ofstream write_file;
+        write_file.open(path, ofstream::trunc);
+
+        if(write_file)
+        {
+            if(!data["tasks"].is_null())
+            {
+                try
+                {
+                    write_file << data.dump(4);
+                    write_file.close();
+
+                    return 0;
+                }
+                catch (const exception& e)
+                {
+                    cerr << "Error writting data to: "
+                        << path << endl;
+                    cerr << e.what() << endl;
+
+                    return -1;
+                }
+            }
+            else
+            {
+                cerr << "There was something wrong with tasks data. " << endl 
+                    << "You can either try correcting the file at "
+                    << path
+                    << " (not recommended) " << endl
+                    << "or regenerating tasks data using "
+                    << argv[0]
+                    << " reset";
+
+                return -1;
+            }
+        }
+        else
+        {
+            cerr << "Unable to open tasks data at " 
+                << path << endl;
+
+            return -1;
+        }
+    }
+
+    else
+    {
+        cerr << argv[1] << " take exactly 2 argument but "
+            << (argc - 2)
+            << " was provided." << endl;
+
+        return -1;
+    }
+}
+
 int update(int argc, char* argv[], const string path, string time)
 {
     if(argc == 4) 
@@ -364,7 +517,7 @@ int update(int argc, char* argv[], const string path, string time)
                 else
                 {
                     cerr << "There was something wrong with the tasks data" << endl;
-                    
+
                     return -1;
                 }
             }
@@ -452,12 +605,12 @@ int main(int argc, char* argv[])
         "remove",   //done
         "update",   //done
 
+        "markd",    //done
+        "markw",    //done
+
         "list",
         "listw",
         "listd",
-
-        "markd",
-        "markw",
 
         "clear",
         "reset"
@@ -483,6 +636,10 @@ int main(int argc, char* argv[])
         else if(string(argv[1]) == "update")
         {
             update(argc, argv, path, time);
+        }
+        else if(string(argv[1]) == "markd" || string(argv[1]) == "markw")
+        {
+            markdw(argc, argv, path, time);
         }
         else
         {}
